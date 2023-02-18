b703e2e68ccd4e325d456aae6b6887e067a65f9a
Boris <drb0r1sdev@gmail.com>
2023-02-18 18:43:14 +0100

    multipleLines group update

--- grammar/symbolGroups/list/multipleLines.js
+++ grammar/symbolGroups/list/multipleLines.js
@@ -19,7 +19,6 @@ export default function multipleLines({ content, symbol, matches, tags }) {
             pairs.classic = [];
             
             const currentMatches = getMatches();
-            console.log(currentMatches)
 
             if(i === 0) {
                 checkPairs(currentMatches);
